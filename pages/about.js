3622cb179a7c19a63fd587d3b23ff430fd1a4994
StevenDRiggs <sfriggs21@gmail.com>
2021-01-06 04:36:36 -0700

    begin add profile pic to about

--- pages/about.js
+++ pages/about.js
@@ -7,13 +7,11 @@ const About = () => {
   return (
     <article>
       <h2>About Me</h2>
-      <Image
+      <img
         src='/Steven_Riggs_photo.jpg'
         alt='Steven Riggs'
-        width={150}
-        height={200}
         className={styles.photo} />
-      <p>I am a self-taught and formally educated full stack software developer with a current emphasis on web applications. I design from the ground up, and really enjoy coming up with non-standard solutions to precisely meet the needs of my clients.</p>
+      <span>I am a self-taught and formally educated full stack software developer with a current emphasis on web applications. I design from the ground up, and really enjoy coming up with non-standard solutions to precisely meet the needs of my clients.</span>
     </article>
   )
 }
