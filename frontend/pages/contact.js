e181ff96bae4f344cfaac48f8e62d2182061522b
StevenDRiggs <sfriggs21@gmail.com>
2021-01-06 16:16:18 -0700

    added Rails backend

--- frontend/pages/contact.js
+++ frontend/pages/contact.js
@@ -6,6 +6,7 @@ import styles from '../styles/Contact.module.css'
 const initialState = {
   name: '',
   email: '',
+  subject: '',
   message: '',
   response: true,
 }
@@ -32,16 +33,18 @@ class Contact extends Component {
 
   handleSubmit = event => {
     event.preventDefault()
+    const { name, email, subject, message, response } = this.state
   }
 
   render() {
-    const { name, email, message, response } = this.state
+    const { name, email, subject,  message, response } = this.state
     return (
       <form className={styles.contactForm} onSubmit={this.handleSubmit}>
         <fieldset>
-          <input type='text' name='name' onChange={this.handleChange} value={name} placeholder='Name' /><br />
-          <input type='email' name='email' onChange={this.handleChange} value={email} placeholder='Email' /><br />
-          <textarea name='message' onChange={this.handleChange} value={message} placeholder='What can I help you with?' /><br />
+          <input type='text' name='name' onChange={this.handleChange} value={name} placeholder='Name' required /><br />
+          <input type='email' name='email' onChange={this.handleChange} value={email} placeholder='Email' required /><br />
+          <input type='text' name='subject' onChange={this.handleChange} value={subject} placeholder='Subject' required /><br />
+          <textarea name='message' onChange={this.handleChange} value={message} placeholder='What can I help you with?' required /><br />
 
           <label htmlFor='responseCheckbox'>Would you like a response?</label>
           <input type='checkbox' name='response' id='responseCheckbox' onChange={this.handleCheckbox} checked={response} />
