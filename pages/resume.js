f32a424b103f80b48929e5c292e1796fc67c332c
StevenDRiggs <sfriggs21@gmail.com>
2021-01-06 06:11:14 -0700

    added resume download to resume page

--- pages/resume.js
+++ pages/resume.js
@@ -1,12 +1,20 @@
+import React, { Component } from 'react'
+
 import styles from '../styles/Resume.module.css'
 
 
-const Resume = () => {
-  return (
-    <article>
-      <iframe src='https://docs.google.com/document/d/e/2PACX-1vSDy7V_2jS4XrzExfGiq12rP90TiK2KM2J1UzdRgkXnJNH-p5U7IZavSjl6WfYQfOvskCeqnNQwKPx_/pub?embedded=true' className={styles.resume} />
-    </article>
-  )
+class Resume extends Component {
+  componentDidMount() {
+    window.open('/Steven_Riggs_Resume.pdf', '_blank', 'download')
+  }
+
+  render() {
+    return (
+      <article>
+        <iframe src='https://docs.google.com/document/d/e/2PACX-1vSDy7V_2jS4XrzExfGiq12rP90TiK2KM2J1UzdRgkXnJNH-p5U7IZavSjl6WfYQfOvskCeqnNQwKPx_/pub?embedded=true' className={styles.resume} />
+      </article>
+    )
+  }
 }
 
 
