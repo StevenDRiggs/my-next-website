e935afc73ae236abd9926efde5491afa3449ba48
StevenDRiggs <sfriggs21@gmail.com>
2021-01-15 05:38:02 -0700

    removed backend for submodule prep

--- backend/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :commentors
-  resources :comments
-  resources :posts
-
-  post '/contact', to: 'application#contact'
-  post 'resume', to: 'application#resume'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
--- backend/db/migrate/20210111020239_add_columns_to_posts.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToPosts < ActiveRecord::Migration[6.0]
-  def change
-    add_column :posts, :title, :string
-    add_colum :posts, :content, :text
-  end
-end
